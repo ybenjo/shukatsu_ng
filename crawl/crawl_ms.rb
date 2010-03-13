946b34ea1484cf143042a2dbbfdf2d50e1c95ba9
benjo kusomushit <y_benjo@g86.local>
2010-03-13 19:04:50 +0900

    initializeの時点でログイン処理する

--- crawl/crawl_ms.rb
+++ crawl/crawl_ms.rb
@@ -18,13 +18,11 @@ class Minshu
       @id = f.gets.chomp
       @pass = f.gets.chomp
     end
-
+    _login
   end
 
   def _login
     login_form = @page.forms[3]
-    p @id
-    p @pass
     login_form['u'] = @id.toutf8
     login_form['p'] = @pass.toutf8
     @page = @agent.submit(login_form)
@@ -32,7 +30,6 @@ class Minshu
     #リダイレクトを待つのがわからないので
     @page = @agent.get("https://www.nikki.ne.jp/a/login/")
     @page.encoding = "UTF-8"
-    puts @page.body.toutf8
   end
 
   def get
