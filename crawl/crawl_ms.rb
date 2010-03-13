18717a4fc874ae568a0117925d8e2a13b87d2822
benjo kusomushit <y_benjo@g86.local>
2010-03-13 19:00:58 +0900

    ログインした

--- crawl/crawl_ms.rb
+++ crawl/crawl_ms.rb
@@ -1,6 +1,45 @@
+#! ruby -Ku
+# -*- coding: utf-8 -*-
+
+require 'kconv'
 require 'rubygems'
 require 'mechanize'
 require 'hpricot'
 
-agent = WWW::Mechanize.new
-login_page = agent.get("https://www.nikki.ne.jp/a/login/")
+class Minshu
+  def initialize(path)
+    @agent = WWW::Mechanize.new
+    # @agent.user_agent_alias = 'Mac Safari'
+    
+    @page = @agent.get("https://www.nikki.ne.jp/a/login/")
+    @page.encoding = "UTF-8"
+    
+    open("#{path}/config") do |f|
+      @id = f.gets.chomp
+      @pass = f.gets.chomp
+    end
+
+  end
+
+  def _login
+    login_form = @page.forms[3]
+    p @id
+    p @pass
+    login_form['u'] = @id.toutf8
+    login_form['p'] = @pass.toutf8
+    @page = @agent.submit(login_form)
+    
+    #リダイレクトを待つのがわからないので
+    @page = @agent.get("https://www.nikki.ne.jp/a/login/")
+    @page.encoding = "UTF-8"
+    puts @page.body.toutf8
+  end
+
+  def get
+  end
+  
+end
+
+if __FILE__ == $0
+  m = Minshu.new(File.dirname(__FILE__))
+end
